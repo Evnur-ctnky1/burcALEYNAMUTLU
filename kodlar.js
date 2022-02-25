1fab9e910e0cd98491a2f88c4c6801c13186e4c9
Evnur-ctnky1 <90348760+Evnur-ctnky1@users.noreply.github.com>
2022-02-25 23:29:59 +0300

    Add files via upload

---
+++ kodlar.js
@@ -0,0 +1,81 @@
+function merhaba(){
+    let ad,soyad,yas,burc;
+    ad=document.getElementById("txtAd").value;
+    yas=document.getElementById("txtSoyad").value;
+    burc=document.getElementById("txtburc").selecedIndex;
+    console.log(burc);
+    document.getElementById("sonucbaslık").innerHTML="";
+    document.getElementById("yazi").innerHTML="";
+}
+if(burc==1){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ OĞLAK HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==2){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ KOVA HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==3){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ BALIK HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==4){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ KOÇ HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==5){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ BOĞA  HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==6){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ İKİZLER HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==7){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ YENGEÇ HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==8){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ ASLAN HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==9){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ BAŞAK HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==10){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ TERAZİ HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==11){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ AKREP HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
+if(burc==12){
+    document.body.style.backgroundColor="#000";
+    yazi.innerHTML="BURCUNUZ YAY HAYATA GÜLÜMSEYİN =)"
+   document.getElementById("İMAGES").setAttribute("src","")
+
+}
